975955eac09cac60ee1f4a0f2a10a566330cb1f3
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-12 15:47:17 -0500

    finished 1-1

--- crackingTheCodingInterview/challenges/a1x1isUnique.java
+++ crackingTheCodingInterview/challenges/a1x1isUnique.java
@@ -1,2 +1,14 @@
+import java.util.HashSet;
+
 public class a1x1isUnique {
+    public static boolean hasUniqueChars(String s){
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0 ; i<s.length(); i++){
+            char currentChar = s.charAt(i);
+            if(set.contains(currentChar)) return false;
+            set.add(currentChar);
+        }
+        return true;
+    }
+
 }
---
+++ crackingTheCodingInterview/challenges/a1x1isUniqueTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class a1x1isUniqueTest extends a1x1isUnique {
+    @Test
+    public void testOne(){
+        assertTrue(hasUniqueChars("abc"));
+    }
+}
