a5e20f300f1ce0482d37ac9dd39cf028ba7eba7b
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-16 11:46:45 -0500

    1-5 finished, imma check again

--- crackingTheCodingInterview/challenges/a1x5OneModificationAwayTest.java
+++ crackingTheCodingInterview/challenges/a1x5OneModificationAwayTest.java
@@ -10,5 +10,6 @@ public class a1x5OneModificationAwayTest extends a1x5OneModificationAway {
         assertFalse(areOneAway("pale","bake"));
         assertTrue(areOneAway("b", ""));
         assertTrue(areOneAway("acca", "akca"));
+        assertFalse(areOneAway("palee","pal"));
     }
 }
