a0f663151e56ec566c6acc8b67a5783cf3206eea
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-17 18:55:28 -0500

    1-6 finished all tests passed

--- crackingTheCodingInterview/challenges/a1x6stringCompressionTest.java
+++ crackingTheCodingInterview/challenges/a1x6stringCompressionTest.java
@@ -5,7 +5,7 @@ public class a1x6stringCompressionTest extends a1x6stringCompression {
     @Test
     public void test1(){
         assertEquals(compressString("aabcccccaaa"),"a2b1c5a3");
-        //
+        //it's working wow
         System.out.println(compressString("aabcccccaaa"));
         assertEquals(compressString("abc"), "abc");
     }
