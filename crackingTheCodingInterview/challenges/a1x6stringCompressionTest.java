44b7dde707feefade67949d95498732fe3da23d5
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-17 19:12:56 -0500

    1-6 in a more efficient manner

--- crackingTheCodingInterview/challenges/a1x6stringCompressionTest.java
+++ crackingTheCodingInterview/challenges/a1x6stringCompressionTest.java
@@ -9,5 +9,7 @@ public class a1x6stringCompressionTest extends a1x6stringCompression {
         System.out.println(compressString("aabcccccaaa"));
         assertEquals(compressString("abc"), "abc");
         //how to make more efficient?
+        //maybe count the number of different chunks of characters
+        //and then see if that number times 2 is less than the length of the string
     }
 }
