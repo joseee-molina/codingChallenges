e6dfc737e94cc51d8c162bd749638808c249562d
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-20 19:22:01 -0500

    finished 1-9 in 3 lines and passed weird test cases

--- crackingTheCodingInterview/challenges/a1x9stringRotation.java
+++ crackingTheCodingInterview/challenges/a1x9stringRotation.java
@@ -1,6 +1,7 @@
 public class a1x9stringRotation {
 
     public static boolean isStringRotation(String s1, String s2){
+        if(s1.length()!=s2.length()) return false;
         String text = s1+s1;
         return text.contains(s2);
     }
---
+++ crackingTheCodingInterview/challenges/a1x9stringRotationTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+public class a1x9stringRotationTest extends a1x9stringRotation {
+    @Test
+    public void test1(){
+        assertTrue(isStringRotation("mypencil","encilmyp"));
+        //passed the test
+        assertFalse(isStringRotation("abcde","abcdf"));
+        assertFalse(isStringRotation("abcde","abcd"));
+
+    }
+}
