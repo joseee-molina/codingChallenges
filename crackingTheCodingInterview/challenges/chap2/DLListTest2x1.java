cd3b5acbe827952221c7b2774811f3cddf7224c7
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-21 17:00:12 -0500

    finished 2-1 and passed test cases

--- crackingTheCodingInterview/challenges/chap2/DLListTest2x1.java
+++ crackingTheCodingInterview/challenges/chap2/DLListTest2x1.java
@@ -10,5 +10,9 @@ public class DLListTest2x1 extends DLList {
         DLList dll = DLList.of(1,2,3,2,4,4,5,11);
         dll.removeDuplicatesFromLinkedList();
         System.out.println(dll.toString());
+        //another test
+        DLList dll2 = DLList.of(1,1,1,1,1,2);
+        dll2.removeDuplicatesFromLinkedList();
+        System.out.println(dll2.toString());
     }
 }
