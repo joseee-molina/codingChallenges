e3faeeec9b838f760dd419056ac44301342be099
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-26 11:15:40 -0500

    passed all tests for both 2-6 in doubly and singly linked lists, which was a cool exercise i beleive

--- crackingTheCodingInterview/challenges/chap2/DLListTest2x6.java
+++ crackingTheCodingInterview/challenges/chap2/DLListTest2x6.java
@@ -15,6 +15,24 @@ public class DLListTest2x6 extends DLList {
     }
 
     @Test
-    
+    public void palindromeFirstMethodTest(){
+        DLList dll = DLList.of(1,2,3,3,2,1);
+        assertTrue(dll.checkIfLinkedListIsPalindromeDoublyLinkedList());
+        DLList dll2 = DLList.of(1,2,1);
+        assertTrue(dll2.checkIfLinkedListIsPalindromeDoublyLinkedList());
+        dll2.addLast(1);
+        assertFalse(dll2.checkIfLinkedListIsPalindromeDoublyLinkedList());
+    }
+
+    @Test
+    public void palindromeSecondMethodTest(){
+        DLList dll = DLList.of(1,2,3,4,3,2,1);
+        assertTrue(dll.checkIfLinkedListIsPalindromeSinglyLinkedList());
+        DLList dll2 = DLList.of(1,2,3,3,2,1);
+        assertTrue(dll2.checkIfLinkedListIsPalindromeSinglyLinkedList());
+        dll2.addLast(2);
+        dll2.addLast(1);
+        assertFalse(dll2.checkIfLinkedListIsPalindromeSinglyLinkedList());
+    }
 
 }
