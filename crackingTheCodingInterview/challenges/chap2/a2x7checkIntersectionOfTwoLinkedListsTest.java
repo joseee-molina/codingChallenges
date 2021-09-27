6e49d0f310146e3be39ec0a59243ea2db050f8b5
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-27 11:20:00 -0500

    finished 2-6 in linear time and constant space complexity, the test passed as well normally

--- crackingTheCodingInterview/challenges/chap2/a2x7checkIntersectionOfTwoLinkedListsTest.java
+++ crackingTheCodingInterview/challenges/chap2/a2x7checkIntersectionOfTwoLinkedListsTest.java
@@ -9,7 +9,7 @@ public class a2x7checkIntersectionOfTwoLinkedListsTest extends a2x7checkIntersec
     public void test1(){
         DLListPublic dll1 = DLListPublic.of(1,2,3);
         DLListPublic dll2 = DLListPublic.of(2,4);
-        dll2.sentinel.prev = dll1.sentinel.next;
-        checkIntersection(dll1,dll2);
+        dll2.sentinel.prev.next = dll1.sentinel.next;
+        assertEquals(checkIntersection(dll1,dll2).item, 1);
     }
 }
