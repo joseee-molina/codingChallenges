279de7303beabc0a3a190bb3cfc897fdc05d9c6c
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-27 11:14:49 -0500

    passed the test for 2-6, let mecheck the debugger if it is ok

--- crackingTheCodingInterview/challenges/chap2/a2x7checkIntersectionOfTwoLinkedListsTest.java
+++ crackingTheCodingInterview/challenges/chap2/a2x7checkIntersectionOfTwoLinkedListsTest.java
@@ -1,7 +1,15 @@
 package chap2;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class a2x7checkIntersectionOfTwoLinkedListsTest extends a2x7checkIntersectionOfTwoLinkedLists {
-
+public class a2x7checkIntersectionOfTwoLinkedListsTest extends a2x7checkIntersectionOfTwoLinkedLists {
+    @Test
+    public void test1(){
+        DLListPublic dll1 = DLListPublic.of(1,2,3);
+        DLListPublic dll2 = DLListPublic.of(2,4);
+        dll2.sentinel.prev = dll1.sentinel.next;
+        checkIntersection(dll1,dll2);
+    }
 }
