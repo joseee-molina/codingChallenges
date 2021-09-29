b07941bae7313dfe66b34d4ad16b214fe55a21f7
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-09-28 23:01:30 -0500

    added test file for 2-8

---
+++ crackingTheCodingInterview/challenges/chap2/a2x8LoopStartingPointTest.java
@@ -0,0 +1,13 @@
+package chap2;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class a2x8LoopStartingPointTest extends DLListPublic {
+    @Test
+    public void test1(){
+        
+    }
+
+}
