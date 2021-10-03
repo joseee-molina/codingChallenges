bb53bcfabcf16fe1f3ba6cca9607781597f7e487
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-03 13:18:23 -0500

    finished 3-5 and it seems cool, let's see what happens with the test

---
+++ crackingTheCodingInterview/challenges/chap3/a3x5SortAStackUsingTwoStacks.java
@@ -0,0 +1,44 @@
+package chap3;
+
+import java.util.Comparator;
+import java.util.Stack;
+
+public class a3x5SortAStackUsingTwoStacks {
+
+    public Stack sortStack(Stack stack) {
+
+        Stack helperStack = new Stack<>();
+
+        while (!stack.isEmpty()) {
+            helperStack.push(findStackMaximum(stack));
+        }
+
+        return helperStack;
+    }
+
+    public int findStackMaximum(Stack<Integer> stack){
+        Stack helper = new Stack();
+        int max = Integer.MIN_VALUE;
+        while(!stack.isEmpty()){
+            int curr = stack.pop();
+            max = curr>max ? curr : max;
+            helper.push(curr);
+        }
+        fillStackAwithStackB(stack, helper, max);
+        return max;
+    }
+
+    public void fillStackAwithStackB(Stack<Integer> a, Stack<Integer> b, int except){
+        boolean excluded = false;
+        while(!b.isEmpty()){
+            int curr = b.pop();
+            if(curr==except && excluded==false){
+                excluded= true;
+                continue;
+            }
+            a.push(curr);
+        }
+    }
+
+
+}
