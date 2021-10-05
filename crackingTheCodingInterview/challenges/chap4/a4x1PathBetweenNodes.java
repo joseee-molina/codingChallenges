ad5f5d33b8d7680b8b29bad4c271c1de3d28337f
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-05 15:13:11 -0500

    idea of 3-4 is finished, tomorrow i will finish the onboarding for trees and graphs and i may continue with it

--- crackingTheCodingInterview/challenges/chap4/a4x1PathBetweenNodes.java
+++ crackingTheCodingInterview/challenges/chap4/a4x1PathBetweenNodes.java
@@ -1,4 +1,12 @@
 package chap4;
 
 public class a4x1PathBetweenNodes {
+    /**To find the path between these two, we can make a queue and add the nonvisited nodes
+     * starting from S and ending at E, and then if there is E in some visited node, then
+     * we return the path.
+     * If not possible, then we just don't do anything
+     * right?
+     *
+     */
+
 }
