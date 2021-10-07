496d70eee04f64342316aa70a351a7a09e61432d
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-07 10:03:09 -0500

    there is an infinite loop in minBST, lets check

---
+++ crackingTheCodingInterview/challenges/chap4/BinaryTreeTest.java
@@ -0,0 +1,15 @@
+package chap4;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinaryTreeTest extends BinaryTree {
+    @Test
+    public void minimumBSTTest(){
+        int[] nums = {1,2,3,4,5,6,7};
+        BinaryTree bst = minimumBST(nums);
+        bst.printInorder();
+    }
+
+}
