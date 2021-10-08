1af60aabca2890753b86183b9a7111a0b08d47d1
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-08 13:32:12 -0500

    finished 4-3 and apssed all test cases

--- crackingTheCodingInterview/challenges/chap4/a3x3ListOfDepths.java
+++ crackingTheCodingInterview/challenges/chap4/a3x3ListOfDepths.java
@@ -18,7 +18,7 @@ public class a3x3ListOfDepths extends BinaryTree{
 
     }
 
-    public ArrayList<DLListPublic<TreeNode>> NodesPerLevel(BinaryTree bt){
+    public ArrayList<DLListPublic<TreeNode>> nodesPerLevel(BinaryTree bt){
         TreeNode root = bt.root;
         ArrayList<DLListPublic<TreeNode>> list = new ArrayList<>();
         DLListPublic<TreeNode> firstList = new DLListPublic();
@@ -38,10 +38,10 @@ public class a3x3ListOfDepths extends BinaryTree{
             DLListPublic nextList = new DLListPublic();
             for(int i = 0 ; i<currentList.size;i++){
                 TreeNode curr = (TreeNode) currentList.get(i);
-                if(!curr.left.equals(null)){
+                if(curr.left!=null){
                     nextList.addLast(curr.left);
                 }
-                if(!curr.right.equals(null)){
+                if(curr.right!=null){
                     nextList.addLast(curr.right);
                 }
             }
--- crackingTheCodingInterview/challenges/chap4/a3x3ListOfDepthsTest.java
+++ crackingTheCodingInterview/challenges/chap4/a3x3ListOfDepthsTest.java
@@ -3,6 +3,8 @@ package chap4;
 import com.sun.source.tree.Tree;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class a3x3ListOfDepthsTest extends a3x3ListOfDepths {
@@ -15,6 +17,9 @@ public class a3x3ListOfDepthsTest extends a3x3ListOfDepths {
         root.left.right = new TreeNode(4);
         root.left.left.right = new TreeNode(5);
         root.left.left.right.left = new TreeNode(6);
-
+        root.left.left.right.right = new TreeNode(7);
+        root.right = new TreeNode(8);
+        ArrayList arr = nodesPerLevel(bt);
+        printListsPerLevel(arr);
     }
 }
