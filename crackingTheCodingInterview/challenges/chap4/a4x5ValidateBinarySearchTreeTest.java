03588b293b5726e86c3faff3bc3e503876a21d24
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-10 10:57:55 -0500

    finished 4-5 in o(n) time and O(n) space, and succesfully passed all testcases, now lets try the edge cases

--- crackingTheCodingInterview/challenges/chap4/a4x5ValidateBinarySearchTreeTest.java
+++ crackingTheCodingInterview/challenges/chap4/a4x5ValidateBinarySearchTreeTest.java
@@ -7,6 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class a4x5ValidateBinarySearchTreeTest extends a4x5ValidateBinarySearchTree {
 
     @Test
+    public void testisBST(){
+        TreeNode root = new TreeNode(3);
+        BinaryTree bt = new BinaryTree(root);
+        root.left= new TreeNode(1);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+        assertTrue(isBinarySearchTree(bt));
+        root.left.left.left = new TreeNode(2);
+        assertFalse(isBinarySearchTree(bt));
 
+    }
+
+    @Test
+    public void testisBSTEdgeCases(){
+        
+    }
 
 }
