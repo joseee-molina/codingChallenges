4867a622db884b9fe849ef3caa7a793691d720f3
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-13 12:22:32 -0500

    added file for 4-8 and an explanation of my solution and coded the solution, it seems to be fine and also added the set parent method, in case the parents are not set, and also added a test file

---
+++ crackingTheCodingInterview/challenges/chap4/a4x8FirstCommonAncestorTest.java
@@ -0,0 +1,14 @@
+package chap4;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class a4x8FirstCommonAncestorTest extends a4x8FirstCommonAncestor {
+
+    @Test
+    public void test1(){
+        
+    }
+
+}
