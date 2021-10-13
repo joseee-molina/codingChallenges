b7d161048037a629360a674d8a8efe141c4a0b64
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-13 12:05:01 -0500

    added file for 4-8 and an explanation of my solution

--- crackingTheCodingInterview/challenges/chap4/a4x8FirstCommonAncestor.java
+++ crackingTheCodingInterview/challenges/chap4/a4x8FirstCommonAncestor.java
@@ -1,4 +1,13 @@
 package chap4;
 
 public class a4x8FirstCommonAncestor {
+
+    /**
+     * The idea is that when you have two nodes in the same level, you can go up
+     * simultaneously and then the first node that is equal on those two pointers is going
+     * to be the first common ancestor. However, there was no "parent" attribute mentioned,
+     * so I doubt that implementation is possible here. In that case, twe have an O(N) time
+     * complexity and an O(1) space complexity
+     */
+
 }
