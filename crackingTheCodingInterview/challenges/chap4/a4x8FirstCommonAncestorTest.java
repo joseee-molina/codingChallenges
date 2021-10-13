f18765d587d741994225ac3ff2132df8343a05b7
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-13 12:28:01 -0500

    added file for 4-8 and an explanation of my solution and coded the solution, it seems to be fine and also added the set parent method, in case the parents are not set, and also added a test file, and coded the test, but there is an error

--- crackingTheCodingInterview/challenges/chap4/a4x8FirstCommonAncestorTest.java
+++ crackingTheCodingInterview/challenges/chap4/a4x8FirstCommonAncestorTest.java
@@ -4,11 +4,23 @@ import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class a4x8FirstCommonAncestorTest extends a4x8FirstCommonAncestor {
+public class a4x8FirstCommonAncestorTest extends a4x8FirstCommonAncestor {
 
     @Test
     public void test1(){
-        
+        TreeNode root = new TreeNode(0);
+        BinaryTree bt = new BinaryTree(root);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(6);
+        root.left.right.right = new TreeNode(9);
+        root.left.right.left.right = new TreeNode(7);
+        TreeNode n1 = root.left.right.right;
+        TreeNode n2 = root.left.right.left.right;
+
+        getFirstCommonAncestor(n1,n2,bt);
+
     }
 
 }
