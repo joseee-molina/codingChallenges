25150990513c1f6c90b0fcaf4b2b69cf7b9d98d2
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-15 01:50:07 -0500

    sorry for not posting yesterday, explanation of the problem is already posted in the file

---
+++ crackingTheCodingInterview/challenges/chap4/a4x9DifferentArraysSameBST.java
@@ -0,0 +1,15 @@
+package chap4;
+
+public class a4x9DifferentArraysSameBST {
+    /**
+     *
+     * Sorry for not posting yesterday! It was a difficult day for me,
+     * I had some problem here in the family but we're all good thankfully.
+     * Or at least i think so. Well, going back to the problem, how to get
+     * all the possible arrays that read from left to right give you some
+     * binary search tree? The idea is in the levels, as long as you take
+     * numbers from the same levels, you can shuffle them as you wish,
+     * and the outputted bst will be the same
+     *
+     */
+}
