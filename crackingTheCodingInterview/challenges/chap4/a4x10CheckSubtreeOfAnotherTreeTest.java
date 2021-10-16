538502a6a1d8aece1bdf2a341757073d720fb2e3
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-16 12:16:08 -0500

    finished 4-10, added a method to know if two trees are equal, and included a test file

---
+++ crackingTheCodingInterview/challenges/chap4/a4x10CheckSubtreeOfAnotherTreeTest.java
@@ -0,0 +1,14 @@
+package chap4;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class a4x10CheckSubtreeOfAnotherTreeTest extends a4x10CheckSubtreeOfAnotherTree {
+
+    @Test
+    public void test1(){
+        
+    }
+
+}
