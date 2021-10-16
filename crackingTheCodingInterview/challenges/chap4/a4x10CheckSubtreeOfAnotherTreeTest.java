bcb7f711f6baba9a11858ebe9c4053fe670aeb9d
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-16 12:47:31 -0500

    finished 4-10, added a method to know if two trees are equal, and included a test file and added a simple testcase. It worked and it is the expected result for a small example, now added a bigger tree and it is working now. Added explnationa bout compelxities

--- crackingTheCodingInterview/challenges/chap4/a4x10CheckSubtreeOfAnotherTreeTest.java
+++ crackingTheCodingInterview/challenges/chap4/a4x10CheckSubtreeOfAnotherTreeTest.java
@@ -28,6 +28,11 @@ public class a4x10CheckSubtreeOfAnotherTreeTest extends a4x10CheckSubtreeOfAnoth
          * Checking equality
          */
         assertTrue(checkSubtree(bt1,bt2));
+        /**
+         * Complexity:
+         * O(N1*N2) time, where N1 is the size of the first tree, and N2 the size of the second
+         * Same for space, since we're doing recursive calls and that takes space
+         */
 
     }
 
