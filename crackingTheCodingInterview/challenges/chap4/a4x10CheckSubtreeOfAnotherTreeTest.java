9275c8ef02cdd885949f596399f29e1765c41b1e
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-16 12:18:54 -0500

    finished 4-10, added a method to know if two trees are equal, and included a test file and added a simple testcase

--- crackingTheCodingInterview/challenges/chap4/a4x10CheckSubtreeOfAnotherTreeTest.java
+++ crackingTheCodingInterview/challenges/chap4/a4x10CheckSubtreeOfAnotherTreeTest.java
@@ -8,7 +8,27 @@ class a4x10CheckSubtreeOfAnotherTreeTest extends a4x10CheckSubtreeOfAnotherTree 
 
     @Test
     public void test1(){
-        
+        /**
+         * First bt
+         */
+        TreeNode root = new TreeNode(0);
+        BinaryTree bt1 = new BinaryTree(root);
+        root.left = new TreeNode(1);
+        root.right=new TreeNode(2);
+
+        /**
+         * Subtree
+         */
+
+        TreeNode root2 = new TreeNode(0);
+        BinaryTree bt2 = new BinaryTree(root2);
+        root2.left = new TreeNode(1);
+
+
+        /**
+         * Checking equality
+         */
+
     }
 
 }
