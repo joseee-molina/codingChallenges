d7c53d69acd0b0c849c8b694ce55f84fb4bb47ad
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-17 11:59:27 -0500

    added file with explanation for 4-11, and finished coding

--- crackingTheCodingInterview/challenges/chap4/a4x11GetRandomNode.java
+++ crackingTheCodingInterview/challenges/chap4/a4x11GetRandomNode.java
@@ -1,6 +1,9 @@
 package chap4;
 
-public class a4x11GetRandomNode {
+import java.util.LinkedList;
+import java.util.Random;
+
+public class a4x11GetRandomNode extends BinaryTree{
     /**
      * The objective here is to get a random node from
      * a binary tree. What I can do is:
@@ -10,6 +13,31 @@ public class a4x11GetRandomNode {
      * 3. Return that node.
      *  Time complexity: O(N)
      *  Space complexity: O(1)
-     *  
+     *
      */
+    public int randNum = 0;
+
+    public TreeNode getRandomNode(BinaryTree bt){
+        TreeNode root = bt.root;
+        LinkedList<TreeNode> llst = new LinkedList<>();
+        Random randomGen = new Random(32);
+        this.randNum = randomGen.nextInt(bt.size);
+        getRandomNodeHelper(root,llst);
+        return
+    }
+
+    private void getRandomNodeHelper(TreeNode node, LinkedList llst){
+        if(randNum==0){
+            llst.addFirst(node);
+        }
+        if(node.left!=null){
+            randNum--;
+            getRandomNodeHelper(node.left,llst);
+        }
+        if(node.right!=null){
+            randNum--;
+            getRandomNodeHelper(node.right,llst);
+        }
+    }
+
 }
