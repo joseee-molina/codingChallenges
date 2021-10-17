bea4f602fefe609cd8ee42c13f5ec5f6d326076f
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-17 11:50:08 -0500

    added file with explanation for 4-11

---
+++ crackingTheCodingInterview/challenges/chap4/a4x11GetRandomNode.java
@@ -0,0 +1,15 @@
+package chap4;
+
+public class a4x11GetRandomNode {
+    /**
+     * The objective here is to get a random node from
+     * a binary tree. What I can do is:
+     * 1. Choose a random number between 0 and the size
+     * of the tree.
+     * 2. Traverse the tree until that number gets to 0.
+     * 3. Return that node.
+     *  Time complexity: O(N)
+     *  Space complexity: O(1)
+     *  
+     */
+}
