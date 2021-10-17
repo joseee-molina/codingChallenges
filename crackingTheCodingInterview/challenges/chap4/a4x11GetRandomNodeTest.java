00ba753694e12eaf74cad76a2e710b7fc9dbe211
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-17 12:03:03 -0500

    added file with explanation for 4-11, and finished coding. Added test file and finished the test1

--- crackingTheCodingInterview/challenges/chap4/a4x11GetRandomNodeTest.java
+++ crackingTheCodingInterview/challenges/chap4/a4x11GetRandomNodeTest.java
@@ -1,8 +1,20 @@
 package chap4;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class a4x11GetRandomNodeTest extends a4x11GetRandomNode {
 
-    
+    @Test
+    public void test1(){
+        TreeNode root = new TreeNode(0);
+        BinaryTree bt = new BinaryTree(root);
+
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        System.out.println(getRandomNode(bt));
+
+    }
 }
