6f4ef262783145e055d662beae19c836169f21a2
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-18 11:26:35 -0500

    added descriptions for 4-12, the last problem in the chapter 4. Finished coding the solution, which is recursive.

--- crackingTheCodingInterview/challenges/chap4/a4x12NumberOfPathsThatAddToANumberInBT.java
+++ crackingTheCodingInterview/challenges/chap4/a4x12NumberOfPathsThatAddToANumberInBT.java
@@ -3,7 +3,36 @@ package chap4;
 public class a4x12NumberOfPathsThatAddToANumberInBT extends BinaryTree{
     /**
      * The problem is to find the number of paths that add up to a certain
-     * number in a binary tree. This problem is a bit challenging.
-     *
+     * number in a binary tree. This problem is a bit challenging. So the
+     * idea is the following.
+     * 1. Have a class variable called "number of paths"
+     * 2. Start from root node traversing every node looking for the correct
+     * sum
+     * 3. Do the same starting from the child nodes of the respective node.
      */
+
+    int numOfPAths;
+
+    public int countNumberOfPathsThatAddUpTo(BinaryTree bt, int n){
+        numOfPAths = 0;
+        countNumberOfPathsHelper(bt.root,0,n);
+        return numOfPAths;
+
+    }
+
+    private void countNumberOfPathsHelper(TreeNode node, int sum, int n){
+        sum+=(int)node.item;
+        if(sum==n){
+            numOfPAths++;
+        }
+        if(node.right!=null){
+            countNumberOfPathsHelper(node.right, sum, n);
+            countNumberOfPathsHelper(node.right,0,n);
+        }
+        if(node.left!=null){
+            countNumberOfPathsHelper(node.left,sum,n);
+            countNumberOfPathsHelper(node.left,0,n);
+        }
+    }
+
 }
