a419ada07b4064484651678f7ce04cfc0c3802fd
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-18 11:27:59 -0500

    added descriptions for 4-12, the last problem in the chapter 4. Finished coding the solution, which is recursive. Added a test file, where I will write a small example first.

---
+++ crackingTheCodingInterview/challenges/chap4/a4x12NumberOfPathsThatAddToANumberInBTTest.java
@@ -0,0 +1,14 @@
+package chap4;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class a4x12NumberOfPathsThatAddToANumberInBTTest extends a4x12NumberOfPathsThatAddToANumberInBT {
+
+    @Test
+    public void test1(){
+        
+    }
+
+}
