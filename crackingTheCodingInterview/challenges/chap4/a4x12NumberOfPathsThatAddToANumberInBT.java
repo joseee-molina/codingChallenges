ab5b21dcf838d600d3982aebe76da9281e9d3c10
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-18 11:15:18 -0500

    added descriptions for 4-12, the last problem in the chapter 4.

---
+++ crackingTheCodingInterview/challenges/chap4/a4x12NumberOfPathsThatAddToANumberInBT.java
@@ -0,0 +1,9 @@
+package chap4;
+
+public class a4x12NumberOfPathsThatAddToANumberInBT extends BinaryTree{
+    /**
+     * The problem is to find the number of paths that add up to a certain
+     * number in a binary tree. This problem is a bit challenging.
+     *
+     */
+}
