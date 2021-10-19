8648f3d938f44cd6244df5b7f843145ee2f10079
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-19 16:17:21 -0500

    added introduction of chapter 6

---
+++ crackingTheCodingInterview/challenges/chap6/a6x0Introduction.java
@@ -0,0 +1,12 @@
+package chap6;
+
+public class a6x0Introduction {
+    /**
+     * Hi! This is the Math and Logic Puzzles Chapter 6
+     *
+     * I am skipping the chapter 5 for now, since I still have
+     * a lot to learn about bit manipulation. This chapter will
+     * be very very fun I'm pretty sure!
+     *
+     */
+}
