00a19a9ccb458fb1c3ba618a8e1021a6edb72b8d
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-19 16:22:37 -0500

    added introduction of chapter 6. Finished 6-1, lets see if the solution was correct

---
+++ crackingTheCodingInterview/challenges/chap6/a6x120BottlesOfPills.java
@@ -0,0 +1,15 @@
+package chap6;
+
+public class a6x120BottlesOfPills {
+    /**
+     * PROBLEM: Given 20 bottles of pills, wfrom which 19 have 1.0 gram pills
+     * and 1 has 1.1 gram pills, find the one that has 1.1 gram pills by using
+     * the scale once. Note: the scale gives you a precise masurement.
+     *
+     */
+
+    /**
+     * My first idea is to put all the pills in the scale, and carefully take one
+     * by one, and take the one that is heavier.
+     */
+}
