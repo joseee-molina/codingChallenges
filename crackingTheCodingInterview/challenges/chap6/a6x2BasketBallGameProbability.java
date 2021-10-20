f5aed47c9d583dc5201e97e54497e40915e351ce
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-20 13:37:52 -0500

    studying probability for finance

---
+++ crackingTheCodingInterview/challenges/chap6/a6x2BasketBallGameProbability.java
@@ -0,0 +1,19 @@
+package chap6;
+
+public class a6x2BasketBallGameProbability {
+    /**
+     * You have a basketball and you can play one of thw two games:
+     * 1. Make one shot and one opportunity.
+     * 2. Make two shots and three opportunities.
+     *
+     * If p is the probability of making one shot, for which values
+     * of p should you choose one or anither?
+     *
+     */
+
+    /**
+     * So this is about probabilities. I have to study probability, so let's
+     * start now! I'm going to the "Finance book" to learn more about that.
+     *
+     */
+}
