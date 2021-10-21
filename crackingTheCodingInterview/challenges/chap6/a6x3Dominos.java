648babd819af39512fab8685551c125bcde34683
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-21 11:44:19 -0500

    added file for 6-3, dominos problem and solved it. Reminded me to a math olympiad problem

--- crackingTheCodingInterview/challenges/chap6/a6x3Dominos.java
+++ crackingTheCodingInterview/challenges/chap6/a6x3Dominos.java
@@ -2,6 +2,12 @@ package chap6;
 
 public class a6x3Dominos {
     /**
-     * There is an 
+     * Is it possible to use 31 dominoes to cover a 8x8
+     * chessboard without 2 opposite corners?
+     *
+     * So, we're gonna quit 2 blacks or 2 whites. Meaning that
+     * we will have 30 blacks and 32 whites or vice versa.
+     * However, we have 31 blacks and 31 whites to cover, so it is
+     * not gonna be possible.
      */
 }
