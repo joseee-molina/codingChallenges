38cdabf035169665407bcd10b1d48117b670228b
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-22 13:45:07 -0500

    finished 6-5, check it out

---
+++ crackingTheCodingInterview/challenges/chap6/a6x5JugsOfWater.java
@@ -0,0 +1,15 @@
+package chap6;
+
+public class a6x5JugsOfWater {
+    /**
+     * We have a container of 3 and 5 Liters and we want to get 4, how do we?
+     */
+
+    /**
+     * Fill 3, pass to 5,
+     * Fill 3, pass to 5 2, we have 1 in 3 remaining,
+     * Empty 5, pass 1 to 5,
+     * fill 3 and pass to 5 which had 1,
+     * so we have 4.
+     */
+}
