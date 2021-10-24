f61e54991f10e2b16947f1a19d703b8cbf192efa
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-24 10:55:17 -0500

    finished and understood 6-6, it was fun

---
+++ crackingTheCodingInterview/challenges/chap6/a6x6BlueEyedIsland.java
@@ -0,0 +1,22 @@
+package chap6;
+
+public class a6x6BlueEyedIsland {
+    /**
+     * There are brown eyed and blue eyed people in the island,
+     * brown eyed people cannot tell them that they are blue eyed,
+     * how much time before all of them elave the island?
+     *
+     * The extreme case is 1 person has blue eyes only. So, that person
+     * looks around and realize that he is the only one, so he goes
+     *  to the plane and leaves in the first day.
+     *
+     *  If two peopla have blue eyes, they see each other but in their minds
+     *  they see only one blue eyed. so they wait to the second day and see that
+     *  the other has not left, so they leave the island.
+     *
+     *  If there are 3, they know that there are 2 or 3 people with blue eyes,
+     *  if there are 2 then they just leave in the second day, but if they dont
+     *  then there are 3.
+     *
+     */
+}
