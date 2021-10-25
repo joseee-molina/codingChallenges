c531c90ebb3bcacef81d49e8cf89318ac1b8b5fa
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-25 13:44:12 -0500

    finished 6-9 using this function. Added test file

--- crackingTheCodingInterview/challenges/chap6/a6x9100LockersTest.java
+++ crackingTheCodingInterview/challenges/chap6/a6x9100LockersTest.java
@@ -1,7 +1,12 @@
 package chap6;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class a6x9100LockersTest extends a6x9100Lockers {
-
+    @Test
+    public void test1(){
+        
+    }
 }
