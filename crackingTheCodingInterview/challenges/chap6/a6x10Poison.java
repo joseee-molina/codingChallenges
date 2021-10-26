72fa663609ba9b73025eea1b46d4b2b243534884
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-26 16:52:46 -0500

    finished 6-10

---
+++ crackingTheCodingInterview/challenges/chap6/a6x10Poison.java
@@ -0,0 +1,14 @@
+package chap6;
+
+public class a6x10Poison {
+    /**
+     * Having 1000 bottles, where 1 is poisoned, we can use 10 poison decetcing strips
+     * but the problem is, that the results come out in 7 days, how can we find the bottle
+     * as quick as possible?
+     */
+
+    /**
+     * We can do a binary search and find the bottle in the 10th trial, or 70 days.
+     * Is that all?
+     */
+}
