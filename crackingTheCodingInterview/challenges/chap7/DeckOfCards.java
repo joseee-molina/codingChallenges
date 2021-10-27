2979517ca4654afc4406a87b7cda03d2d6061e21
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-27 14:04:02 -0500

    starting the chapter 7 wohoo! Object Oriented Design, and finished the deck of cards class

--- crackingTheCodingInterview/challenges/chap7/DeckOfCards.java
+++ crackingTheCodingInterview/challenges/chap7/DeckOfCards.java
@@ -10,3 +10,7 @@ public class DeckOfCards <T> {
         this.deck = deck;
     }
 }
+/**
+ * We can create blackjack by extending the DeckOfCards class and also
+ * even creating the blackjack cards extending the Card class
+ */
