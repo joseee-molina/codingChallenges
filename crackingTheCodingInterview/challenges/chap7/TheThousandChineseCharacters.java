a3d3b440f19acbf677f734dd577b47cb5fc0614d
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-10-29 20:08:55 -0500

    studying for hsk5

---
+++ crackingTheCodingInterview/challenges/chap7/TheThousandChineseCharacters.java
@@ -0,0 +1,10 @@
+package chap7;
+
+public class TheThousandChineseCharacters {
+    /**
+     * Hi, tomorrow I will take my HSK5 exam again, which is why now i a studying
+     * and will not upload a solution.
+     *
+     * Cheers!
+     */
+}
