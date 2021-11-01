319c96e5066df87a4fc3637e0a4c05ce0573c326
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-01 10:40:44 -0600

    adding Jukebox class, and finished it

---
+++ crackingTheCodingInterview/challenges/chap7/JBMusic.java
@@ -0,0 +1,7 @@
+package chap7;
+
+public class JBMusic {
+    String musicName;
+    int cost;
+    int duration;
+}
--- crackingTheCodingInterview/challenges/chap7/Jukebox.java
+++ crackingTheCodingInterview/challenges/chap7/Jukebox.java
@@ -1,8 +1,74 @@
 package chap7;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+
 public class Jukebox {
     /**
      * Design a Jukebox. IDK what this is but imma search.
-     * 
+     * Alright, it's a machine that play a song that you choose
+     * when you insert a Coin
+     *
      */
+
+    /**
+     * I will add a JBMusic class so that I can put there
+     * the attributes of each song, and here I will just
+     * put a hashmap of the songs
+     */
+
+    HashMap<Integer, JBMusic> songs;
+    int numberOfSongs;
+    HashSet<JBMusic> songsSet;
+
+    public Jukebox(){
+        songs = new HashMap<>();
+        numberOfSongs = 0;
+        songsSet = new HashSet<>();
+    }
+
+    public void addSong(JBMusic song){
+        numberOfSongs++;
+        songs.put(numberOfSongs, song);
+        songsSet.add(song);
+    }
+    /**
+     * Now every song has a number, and that way we can configure the JukeBox
+     */
+
+    public void modifyNumberAndSong(int num, JBMusic song){
+        songsSet.remove(songs.get(num));
+        songs.put(num, song);
+        songsSet.add(song);
+    }
+
+    public void removeSong(int num, JBMusic song){
+        songsSet.remove(song);
+        songs.put(num, song);
+    }
+
+    /**
+     * Choose song with a number
+     */
+    public void chooseSong(int num, int money){
+        if(!songs.keySet().contains(num)){
+            throw new IllegalArgumentException();
+        }
+        JBMusic chosen = songs.get(num);
+        if(money> chosen.cost){
+            playSong(chosen);
+        }
+        else{
+            System.out.println("Not enought money man");
+        }
+    }
+
+    public void playSong(JBMusic song){
+        /*
+        Playing...
+         */
+    }
+
+
 }
