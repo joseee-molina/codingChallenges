b4cf71bfde13a41042712e1ecbaf6b37d58b9add
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-02 09:16:58 -0600

    adding basics for ParkingLot

---
+++ crackingTheCodingInterview/challenges/chap7/ParkingLot.java
@@ -0,0 +1,25 @@
+package chap7;
+
+public class ParkingLot {
+    int capacity;
+    ParkingLotSpace[] places;
+
+    public ParkingLot(int cap, ParkingLotSpace[] places){
+        capacity=cap;
+        this.places=places;
+    }
+
+    /**
+     * Places names can be like A1, A2, ... B1, B2, ... and so on
+     *
+     */
+
+    /**
+     * Occupy first unoccupied place
+     */
+    public void occupyPlace(){
+
+    }
+
+
+}
---
+++ crackingTheCodingInterview/challenges/chap7/ParkingLotSpace.java
@@ -0,0 +1,10 @@
+package chap7;
+import java.time.Instant;
+
+public class ParkingLotSpace {
+    public int lenght;
+    public int width;
+    public Instant usedFrom;
+    public String name = "";
+    public boolean occupied;
+}
