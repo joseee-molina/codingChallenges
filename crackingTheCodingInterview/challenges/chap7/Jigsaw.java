09e94f9f633a32e41178342852b94dd887b33404
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-05 19:33:36 -0600

    Sorry for not uploading yesterday! It was kinda crazy. Well, added 7-6: Jigsaw! It is basically an array of pieces.

---
+++ crackingTheCodingInterview/challenges/chap7/Jigsaw.java
@@ -0,0 +1,11 @@
+package chap7;
+
+public class Jigsaw {
+    JigsawPiece[][] pieces;
+    int numberOfPieces;
+
+    public Jigsaw(JigsawPiece[][] pieces, int numberOfPieces){
+        this.numberOfPieces=numberOfPieces;
+        this.pieces=pieces;
+    }
+}
