233b60d451d5a0095ad4ec3c7261f8e74135ea80
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-05 19:36:07 -0600

    Sorry for not uploading yesterday! It was kinda crazy. Well, added 7-6: Jigsaw! It is basically an array of pieces. I added the different edges in the JigsawPiece class

--- crackingTheCodingInterview/challenges/chap7/JigsawPiece.java
+++ crackingTheCodingInterview/challenges/chap7/JigsawPiece.java
@@ -1,4 +1,22 @@
 package chap7;
 
 public class JigsawPiece {
+    /**
+     * To make this easy, there are are a finite kind of edges numbered from 0 to K
+     *
+     */
+
+    int edge0;
+    int edge1;
+    int edge2;
+    int edge3;
+    int edge4;
+
+
+
+    public JigsawPiece(){
+
+    }
+
+    public boolean fitsWith()
 }
