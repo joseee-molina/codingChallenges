deb51228e45568113b314c557f832908de03c0db
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-06 15:04:38 -0600

    Hi! Added 7-7: Chat server. Added explanation of login, register, friends, messaging

--- crackingTheCodingInterview/challenges/chap7/ChatServer.java
+++ crackingTheCodingInterview/challenges/chap7/ChatServer.java
@@ -5,17 +5,25 @@ public class ChatServer {
      * Explain how you would design a chat server. What would be the hardest
      * problem to solve?
      *
+     * Login and register:
      * First of all, you would log in with a username and a password, and after checking
      * those elements are correct in the hashmap maybe, then you would enter the server.
      *
-     * There are an arry of users that you can contact, and for every user in your array of
-     * friends there is actually more data like the conversations youve had with them, the
+     * If you don't have an account, you would create one user with an username, a password,
+     * an email, etc.
+     *
+     * Friends
+     *
+     * There are an array of users that you can contact, and for every user in your array of
+     * friends there is actually more data like the conversations you've had with them, the
      * friends you have in common, and more data about the relationship between you and the
      * friends.
      *
+     * Inbox
+     *
      * There must be an inbox where you have incoming messages.
      *
-     *
+     * 
      *
      * There must be some methods that are relevant:
      * 1. sendMessageToFriend(Friend a, String message){} where you send the "message" to "a"
@@ -23,6 +31,8 @@ public class ChatServer {
      * 3. reply(Message m, String reply){} where you reply to some message.
      *
      * These methods are for messages
+     *
+     *
      *
      */
 }
