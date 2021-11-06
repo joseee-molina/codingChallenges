46cafbb6b693f41555908eb911dbf81462bfca23
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-06 14:59:42 -0600

    Hi! Added 7-7: Chat server. Added explanation of login and friends

--- crackingTheCodingInterview/challenges/chap7/ChatServer.java
+++ crackingTheCodingInterview/challenges/chap7/ChatServer.java
@@ -5,5 +5,21 @@ public class ChatServer {
      * Explain how you would design a chat server. What would be the hardest
      * problem to solve?
      *
+     * First of all, you would log in with a username and a password, and after checking
+     * those elements are correct in the hashmap maybe, then you would enter the server.
+     *
+     * There are an arry of users that you can contact, and for every user in your array of
+     * friends there is actually more data like the conversations youve had with them, the
+     * friends you have in common, and more data about the relationship between you and the
+     * friends.
+     *
+     * There must be an inbox where you have incoming messages.
+     *
+     *
+     *
+     * There must be some methods that are relevant:
+     * 1. sendMessageTO(Friend a, String message){} where you send the "message" to "a"
+     * 2. openMEssage(Message m){} which will output the message m in the terminal.
+     *
      */
 }
