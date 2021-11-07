114e307126fd78e178dd86e433ddbba8a1caec30
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-07 10:21:33 -0600

    added 7-8, othello explanation

---
+++ crackingTheCodingInterview/challenges/chap7/Othello.java
@@ -0,0 +1,10 @@
+package chap7;
+
+public class Othello {
+    /**
+     * The japanese chess is black and white double sided circular pieces,
+     * with a 8x8 board where you place white or black pieces. So I prefer to
+     * include an array of 0, 1, 2 array of the board, where 0 = unoccupied,
+     * 1 = white piece, 2 = black piece in that position of the board
+     */
+}
