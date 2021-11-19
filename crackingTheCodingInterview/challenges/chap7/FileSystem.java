616c5656e7bccd3134c6863097f06c8bef1c8765
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-19 12:22:26 -0600

    adding HashMap class

---
+++ crackingTheCodingInterview/challenges/chap7/FileSystem.java
@@ -0,0 +1,14 @@
+package chap7;
+
+public class FileSystem {
+    /**
+     I would have directories, and those directories can be in a Hashmap, where the original directory has
+     some directories inside it, and you can create more of them. In other words, the Hashmaps have names and those
+     point to other hashmaps (directories).
+
+     In order to get an absolute and a relative path, we can use an iterative approach and start with a /, then
+     whenever we go deeper into the hashmaps, we add another / followed by the name of the directory
+
+
+     */
+}
