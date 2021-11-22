f7e2af8f56b5a429891875e1d8a7706f2fdad5d9
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-22 13:50:01 -0600

    adding description for 8_2

---
+++ crackingTheCodingInterview/challenges/chap8/_8_2_RobotPath.java
@@ -0,0 +1,13 @@
+package chap8;
+
+public class _8_2_RobotPath {
+    /*
+    There is a robot in the top left of a board with r rows and c columns,
+    it can move down and right, and it wants to arrive the bottom right cell.
+    there are however, some cells that he cannot step in, so we need to find
+    a path so that it can arrive its destination.
+
+    ------------------------------------------------------------------------
+    
+     */
+}
