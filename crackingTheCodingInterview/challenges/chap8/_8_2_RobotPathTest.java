78503ca92af3616faf2efd4fe9d2c3ccf03e108d
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-23 15:13:59 -0600

    adding 8_2

--- crackingTheCodingInterview/challenges/chap8/_8_2_RobotPathTest.java
+++ crackingTheCodingInterview/challenges/chap8/_8_2_RobotPathTest.java
@@ -1,7 +1,12 @@
 package chap8;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class _8_2_RobotPathTest extends _8_2_RobotPath {
-
+    @Test
+    public void test1(){
+        
+    }
 }
