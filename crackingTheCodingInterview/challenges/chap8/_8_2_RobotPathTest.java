9ac30ebd7a3b5a4a5a0605a8548c4777843f8214
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-23 15:16:39 -0600

    adding 8_2, it will always find a path, which is cool, but the thing is that idk whatpath that is

--- crackingTheCodingInterview/challenges/chap8/_8_2_RobotPathTest.java
+++ crackingTheCodingInterview/challenges/chap8/_8_2_RobotPathTest.java
@@ -7,6 +7,10 @@ import static org.junit.Assert.*;
 public class _8_2_RobotPathTest extends _8_2_RobotPath {
     @Test
     public void test1(){
-        
+        int[][] board1 = new int[3][4];
+        board1[0][2] = 1;
+        board1[1][1] = 1;
+        print2DArr(board1);
+        findPathToEnd(board1);
     }
 }
