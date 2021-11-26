3466179ae83c1527fa8b226f9a1c7367d6ed3f10
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-26 11:34:40 -0600

    adding 8_4, subsets, but i will think of a better way to solve it

---
+++ crackingTheCodingInterview/challenges/chap8/_8_4_AllSubsetsOfASet.java
@@ -0,0 +1,13 @@
+package chap8;
+
+public class _8_4_AllSubsetsOfASet {
+    /**
+     * This is recursive:
+     * A set of N elements has 2^N subsets, so, essentially, we need to compute
+     * all possible permutations of the elements in the set for length 1,2,...,N
+     * and those permutations will be n
+     * The base case is the empty and the 1 element set:
+     * if set empty, the subset is itself,
+     * if set 1 element, the subsets is the empty and the itself
+     */
+}
