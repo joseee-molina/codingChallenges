1997d4a43f1736912c5ba06b2da8d6a3656f744f
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-28 11:11:30 -0600

    iteration is not good for this, but what is the point of this weird exercise?

--- crackingTheCodingInterview/challenges/chap8/_8_5_MultpiplyWithoutAsterisk.java
+++ crackingTheCodingInterview/challenges/chap8/_8_5_MultpiplyWithoutAsterisk.java
@@ -6,6 +6,8 @@ public class _8_5_MultpiplyWithoutAsterisk {
     One way is to iterate over one integer and sum the other one
     as we iterate, but that may be very slow.
     Let's think of a better way
-     */
+    There is a way to solve this using bit shifting.
     
+     */
+
 }
