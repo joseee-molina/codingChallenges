eb8d9895c967332c027574bdf70fe23fd5550030
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-28 10:31:29 -0600

    iteration is not good for this, but what is the point of this weird exercise?

---
+++ crackingTheCodingInterview/challenges/chap8/_8_5_MultpiplyWithoutAsterisk.java
@@ -0,0 +1,11 @@
+package chap8;
+
+public class _8_5_MultpiplyWithoutAsterisk {
+    /*
+    How to multiply 2 positive integers without using the *?
+    One way is to iterate over one integer and sum the other one
+    as we iterate, but that may be very slow.
+    Let's think of a better way
+     */
+    
+}
