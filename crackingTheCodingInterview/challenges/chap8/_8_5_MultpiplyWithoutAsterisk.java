a49f5df3e21d50595c66527093d26c482e3c5c25
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-29 13:42:40 -0600

    started, but will continue later

--- crackingTheCodingInterview/challenges/chap8/_8_5_MultpiplyWithoutAsterisk.java
+++ crackingTheCodingInterview/challenges/chap8/_8_5_MultpiplyWithoutAsterisk.java
@@ -5,9 +5,10 @@ public class _8_5_MultpiplyWithoutAsterisk {
     How to multiply 2 positive integers without using the *?
     One way is to iterate over one integer and sum the other one
     as we iterate, but that may be very slow.
-    Let's think of a better way
+    Let's think of a better way.
     There is a way to solve this using bit shifting.
-    
+
      */
+
 
 }
---
+++ crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
@@ -0,0 +1,9 @@
+package chap8;
+
+public class _8_6_TowersOfHanoi {
+
+    /*
+    Given N disks and 3 towers (stacks), the disks ca slide into any tower. The puzzle starts with
+    disks
+     */
+}
