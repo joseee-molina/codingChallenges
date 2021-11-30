a4a7fb936c8757a4347d9f7f453580491629c8fb
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-11-30 17:14:37 -0600

    adding description for hanoi towers

--- crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
+++ crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
@@ -3,7 +3,8 @@ package chap8;
 public class _8_6_TowersOfHanoi {
 
     /*
-    Given N disks and 3 towers (stacks), the disks ca slide into any tower. The puzzle starts with
-    disks
+    Given N disks and 3 towers (stacks), the disks can slide into any tower. The disks
+    cannot be placed under smaller ones, and that is the rule. Please sort them using
+    the three towers.
      */
 }
