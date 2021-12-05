2be083376cd87d9283b61dacb307491127005a17
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-04 22:31:46 -0600

    towers of hanoi details

--- crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
+++ crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
@@ -4,10 +4,13 @@ public class _8_6_TowersOfHanoi {
 
     /*
     Given N disks and 3 towers (stacks), the disks can slide into any tower. The disks
-    cannot be placed under smaller ones, and that is the rule. Please sort them using
-    the three towers.
+    cannot be placed under smaller ones, and that is the rule. Please transport them into another tower.
 
-    After playng for a while, there are some details I would like to talk about
+    After playng for a while, there are some details I would like to talk about.
+    The base case is when we have only one disk, then we just move it. When we have two disks, then we move
+    one to tower 3 then the other t 2 then the 3 to 2.
+    And the process is similar for all of the others, it is just that we need to solve it
+    recursively
      */
 }
 
