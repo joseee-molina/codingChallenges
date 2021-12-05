500d0b1d80dee8675f2f958e6d113bf8f51e6d34
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-04 22:30:07 -0600

    towers of hanoi details

--- crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
+++ crackingTheCodingInterview/challenges/chap8/_8_6_TowersOfHanoi.java
@@ -6,5 +6,8 @@ public class _8_6_TowersOfHanoi {
     Given N disks and 3 towers (stacks), the disks can slide into any tower. The disks
     cannot be placed under smaller ones, and that is the rule. Please sort them using
     the three towers.
+
+    After playng for a while, there are some details I would like to talk about
      */
 }
+
