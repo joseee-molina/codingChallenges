19b5c443f942aed4a2b5213aafab02033349d8ce
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-20 22:07:37 -0600

    plato philosophygit add . Good to read if interested

---
+++ crackingTheCodingInterview/challenges/chap8/WesternPhilosophy_Plato_Socrates.java
@@ -0,0 +1,76 @@
+package chap8;
+
+public class WesternPhilosophy_Plato_Socrates {
+    /**
+     * Hi, just in case someone is interested, I'm analyzing the famous
+     * book of plato for my class of Western Philosophy, so I have
+     * to first translate it into spanish and then be able to understand it
+     * before the class discussion in Chinese
+     *
+     */
+    /*
+
+
+
+
+
+
+客人: 除此之外还有其他结合的方式---人的联系。当神性的联系已经造就时,人们 -开始工作就不难看清工作的性质,然后就开始造就人与人之间的联系。
+小苏格拉底: 这种联系是什么?如何造就?
+客人: 这种联系的形成依靠两种类型的人之间的通婚,使通婚后生下的孩子为两种类型的人共有,这种联系的形成还要依靠严禁把婚姻当作个人私事,不把女儿私自嫁出去。从优生的角度看,大部分男人的婚姻都是不恰当的。
+小苏格拉底: 你这是什么意思?
+客人: 在这样的婚姻发生时,人们难道认为不应当批评这种盛行的、以追求财富和名声为目的的婚姻吗?
+小苏格拉底: 不应当,这样做其实也没有什么大错。
+客人: 但是我们特别关心那些想要建立"良好关系"的人,如果我们发现他们的行为不妥,就应当公开地说出来。
+小苏格拉底: 这样说是合理的。
+客人: 他们并没有按照任何健全的或始终--贯的原则行事。你熊,他们对那些与他们相同的人欢呼致意,厌恶与他们不同的人,追求欲望的直接满足。就这样,他们过分强调了与他们自己相。同和不同的重要性。
+小苏格拉底: 以什么方式?
+
+客人: 有节制的人寻找一个与自己品性相似的伴侣,在可能
+的情况下,他们选择網静的女人为妻。当他们有女儿要出嫁时,他们又会寻找与他们相同品性的人做他们的女嬉。具有勇敢品质的人也会这样做,寻找与他们同类的人。尽管这两种类型的人应当做的事情也许正好相反,但这种寻求同类的情况一直在延续。
+小苏格拉底: 他们怎么会这样?为什么他们应当做相反的事?
+客人: 这是因为,如果具有勇敢品性的人经过许多代的繁殖,不与任何具有节制品性的人通婚,那么这种自然发展过程就会走向极端,这种人起初会变得极为强大,但到了最后就会变得极为凶残和疯狂。
+小苏格拉底: 这是可以预料的。
+客人: 但对那些过分节制和谦单,缺乏勇气和大胆的人来说,这种人如果繁殖许多代,就会变得太迟純而难以应对生活的挑战,到了最后就会变得软弱无能了。
+小苏格拉底: 是的,这种结果也是可以预料到的。
+客人: 我再重复一下我的意思。如果神性的联系已经造就,那么要建立这些人性的联系并不困难。这种联系是两种类型的人共同拥有的关于价值标准的信念。当国王想要织造国家之网时,他会有一种清醒的见地。他决不会允许把具有温和品性的人与具有勇敢品性的人分开,为了避免这种情况,他必须把国家织造得密密实实,首先是在各种不同类型的人心中建立起共同的信念,使公众的荣誉和胜利服务于这一目的,最后,每一类型的人必须与其他类型的人建立联系,缔结庄严的婚约。当国王使用不同类型的材料把他的织物织造得像术语所说的那样平滑而又密实的时候,他。必须把管理国家的各种职司托付给他们,在各种情况下让他们分担公务。
+
+小苏格拉底: 他如何能做到这一点?
+客人: 当需要一位执政官的时候,政治家必须选择一名同时拥有两种性格的人,把权力交给他。而在需要几位执政官的时候,他必须从两种类型中分别挑选一些代表,使他们--道分担公务。节制型的执政官是极为谨慎、公正、循规蹈矩的,但他们缺乏勇气和进取心,不能高效率地工作。
+小苏格拉底: 对,你这个总结还是挺公正的。
+客人: 勇敢型的人与前者相比,显得公正与谨慎不足,但在行动的大胆方面优前者。除非这两种品质同时存在和起作用,否则一个国家决不可能在公共和私人事务两方面都发挥良好的作用。
+小苏格拉底: 当然是这样的。
+客人: 现在我们已经把织造国家之网的工作做到头了。在政治家的织造下,这些毛线都在起作用,既有温和型的,又有勇敢型的。这些毛线织成了--种统一的类型。国王的技艺依靠相互和谐与友谊的组带把两种类型的生活织成一种真正的同胞关系,赢得了这种统一。这块织物成为一切织物中最美好的,最优秀的。它把城邦里的所有居民都紧密地联结在一起,无论是奴隶还是自由民。这位国王纺织工保持着他的控制权和监督权,这个国家拥有获取人间幸福生活所需要的一切。
+小苏格拉底: 先生,你已经完成了我们要你做的工作,在被你搁置的智者定义旁边,你为真正的国王和政治家描绘了一幅完美的图画。
+
+Invitado: Además, hay otras formas de combinar --- contacto personal. Cuando se ha creado la conexión divina, las personas, no es difícil ver la naturaleza del trabajo cuando comienzan a trabajar, y luego comienzan a crear la conexión entre las personas.
+Pequeño Sócrates: ¿Qué es esta conexión y cómo se establece?
+Invitado: La formación de esta conexión se basa en el matrimonio mixto entre los dos tipos de personas, de modo que los niños nacidos después del matrimonio mixto sean compartidos por los dos tipos de personas. La formación de esta conexión también depende de la prohibición del matrimonio como un acto personal. No se case con su hija en privado. Desde la perspectiva de la eugenesia, la mayoría de los matrimonios de hombres son inapropiados.
+Pequeño Sócrates: ¿Qué quieres decir?
+Invitado: Cuando ocurre este tipo de matrimonio, ¿no cree la gente que este tipo de matrimonio popular para la búsqueda de la riqueza y la fama no debe ser criticado?
+Pequeño Sócrates: No, en realidad no hay nada de malo en hacer esto.
+Invitado: Pero nos preocupan especialmente aquellos que quieren construir una "buena relación", si encontramos que su comportamiento es inapropiado, debemos decirlo públicamente.
+Pequeño Sócrates: Es razonable decir eso.
+Invitados: No actuaron de acuerdo con principios sólidos o consistentes. Soportan, animan a quienes son como ellos, odian a las personas que son diferentes a ellos y persiguen la gratificación directa de los deseos. De esta forma, sobreenfatizaban el estar consigo mismos. La importancia de lo mismo y la diferencia.
+Pequeño Sócrates: ¿De qué manera?
+
+Invitado: Una persona templada busca una pareja que tenga una personalidad similar a la suya.
+Dadas las circunstancias, eligen a la mujer netamente tranquila como esposa. Cuando tengan una hija para casarse, buscarán personas del mismo carácter que las suyas para que sean sus mamelucos femeninos. Las personas con cualidades valientes harán lo mismo, buscando personas como ellos. Aunque las cosas que deberían hacer estos dos tipos de personas pueden ser exactamente lo contrario, esta situación de buscar el mismo tipo no ha cesado.
+Pequeño Sócrates: ¿Cómo podían ser así ?, ¿Por qué iban a hacer lo contrario?
+Invitado: Esto se debe a que si una persona con carácter valiente pasa por muchas generaciones de reproducción y no se casa con nadie con moderación, entonces este proceso de desarrollo natural llegará a los extremos, y este tipo de persona se volverá extremadamente poderosa al principio, pero en el futuro. Al final se volverá extremadamente cruel y loco.
+Pequeño Sócrates: Es de esperar.
+Invitado: Pero para aquellos que son excesivamente comedidos y modestos, faltos de coraje y audacia, si este tipo de persona se reproduce durante muchas generaciones, será demasiado tarde para ser puro y difícil de enfrentar los desafíos de la vida, y al final se volverá débil e incompetente.
+Pequeño Sócrates: Sí, también se puede esperar este tipo de resultado.
+Invitado: Permítanme repetir lo que quiero decir. Si se han establecido conexiones divinas, entonces no es difícil establecer estas conexiones humanas. Esta conexión es la creencia en valores que comparten ambos tipos de personas. Cuando el rey quiera tejer la red del país, tendrá una perspectiva sobria. Nunca permitirá la separación de personas con cualidades moderadas de aquellas con cualidades valientes. Para evitar esto, debe tejer el país densamente, en primer lugar para establecer un corazón común entre los diferentes tipos de personas. La fe que hace público el honor y la victoria. Finalmente, cada tipo de persona debe establecer contacto con otro tipo de personas y celebrar un contrato matrimonial solemne. Cuando el rey usó diferentes tipos de materiales para tejer sus tejidos tan suaves y densos como dice el término, él. Se les debe encomendar las diversas funciones de administración del país y permitirles compartir los deberes oficiales en diversas circunstancias.
+
+Pequeño Sócrates: ¿Cómo puede hacer esto?
+Invitado: Cuando se necesita un cónsul, el político debe elegir a una persona que tenga ambas personalidades y darle el poder. Cuando se necesiten varios cónsules, debe seleccionar algunos representantes de los dos tipos para que puedan compartir las funciones oficiales. Los cónsules templados son extremadamente cautelosos, justos y obedientes, pero carecen de coraje y espíritu emprendedor y no pueden trabajar de manera eficiente.
+Pequeño Sócrates: Sí, tu resumen es bastante justo.
+Invitado: Comparado con el primero, el valiente parece ser menos justo y cauteloso, pero es superior al primero en términos de audacia de acción. A menos que estas dos cualidades existan y funcionen al mismo tiempo, un país nunca podrá desempeñar un buen papel en los asuntos públicos y privados.
+Pequeño Sócrates: Por supuesto que lo es.
+Invitado: Ahora hemos hecho el trabajo de tejer la web nacional. Bajo el tejido de los políticos, estos hilos de lana están todos en funcionamiento, a la vez suaves y valientes. Estos hilos se tejen en un tipo uniforme. La habilidad del rey se basa en la armonía y la amistad mutuas para tejer los dos tipos de vida en una verdadera relación de compatriotas, que ha ganado esta unidad. Esta pieza de tela se ha convertido en la más hermosa y destacada de todas las telas. Conecta estrechamente a todos los residentes de la ciudad-estado, ya sean esclavos o personas libres. El rey tejedor mantiene sus derechos de control y supervisión Este país tiene todo lo necesario para obtener una vida feliz en la tierra.
+Pequeño Sócrates: Señor, ha hecho lo que queremos que haga. Junto a la definición del sabio que dejó en suspenso, pinta una imagen perfecta del verdadero rey y político.
+
+     */
+
+}
